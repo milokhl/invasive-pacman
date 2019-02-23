0b293da4ea4147e18efb855930f5b2899652e163
Milo Knowles <mknowles@mit.edu>
2019-02-23 17:44:40 -0500

    Basic pac man movement

---
+++ Assets/Scripts/PacManMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacManMove : MonoBehaviour {
+    // Expose the speed parameter to Unity.
+    public float speed = 0.4f;
+    Vector2 dest = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start() {
+        dest = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {  
+    }
+
+    void FixedUpdate() {
+        // Move closer to the current destination.
+        Vector2 updated_pos = Vector2.MoveTowards(transform.position, dest, speed);
+
+        // Move the RigidBody2D rather than setting tranform.position directly.
+        GetComponent<Rigidbody2D>().MovePosition(updated_pos);
+
+        // Check for keyboard input if not moving (at destination).
+        if ((Vector2)transform.position == dest) {
+            if (Input.GetKey(KeyCode.UpArrow) && CollisionFree(Vector2.up)) {
+                dest = (Vector2)transform.position + Vector2.up;
+            }
+            if (Input.GetKey(KeyCode.RightArrow) && CollisionFree(Vector2.right)) {
+                dest = (Vector2)transform.position + Vector2.right;
+            }
+            if (Input.GetKey(KeyCode.DownArrow) && CollisionFree(-Vector2.up)) {
+                dest = (Vector2)transform.position - Vector2.up;
+            }
+            if (Input.GetKey(KeyCode.LeftArrow) && CollisionFree(-Vector2.right)) {
+                dest = (Vector2)transform.position - Vector2.right;
+            }
+        }
+    }
+
+    bool CollisionFree(Vector2 direction) {
+        // Cast Line from 'next to Pac-Man' to 'Pac-Man'
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast(pos + direction, pos);
+        return (hit.collider == GetComponent<Collider2D>());
+    }
+}
