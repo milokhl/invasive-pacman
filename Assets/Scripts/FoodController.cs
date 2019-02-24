3db1bfc82bb51317054d1b54c2cf1f951a9f55fe
Milo Knowles <mknowles@mit.edu>
2019-02-23 19:25:18 -0500

    Collision function

--- Assets/Scripts/FoodController.cs
+++ Assets/Scripts/FoodController.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FoodController : MonoBehaviour {
-    public int foodLevel = 2;
+    public int foodLevel = 10;
 
     // Called whenever something enters the BoxCollider2D of the Food prefab.
-    // void OnTriggerEnter2D(Collider2D co) {
-        // if (co.name == "PacManPlayer") {
-        //     foodLevel -= 1;
-        //     Debug.Log("collide");
-        // }
-    // }
+    void OnTriggerEnter2D(Collider2D co) {
+        if (co.name == "PacManPlayer") {
+            foodLevel -= 1;
+            Debug.Log("collide");
+        }
+    }
 }
