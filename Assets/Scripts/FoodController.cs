4e249dd7ac37e989b2cf5f30cc8be503e2d146c8
Milo Knowles <mknowles@mit.edu>
2019-02-23 19:23:58 -0500

    Add trigger on food without blocking raycast

---
+++ Assets/Scripts/FoodController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodController : MonoBehaviour {
+    public int foodLevel = 2;
+
+    // Called whenever something enters the BoxCollider2D of the Food prefab.
+    // void OnTriggerEnter2D(Collider2D co) {
+        // if (co.name == "PacManPlayer") {
+        //     foodLevel -= 1;
+        //     Debug.Log("collide");
+        // }
+    // }
+}
