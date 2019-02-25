73821a5805277295919a7f2dc7c660800e59b452
Milo Knowles <mknowles@mit.edu>
2019-02-25 15:35:18 -0500

    Two grass types

--- Assets/Scripts/FoodController.cs
+++ Assets/Scripts/FoodController.cs
@@ -3,81 +3,78 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FoodController : MonoBehaviour {
-    // The food level is reset to this every 'regrowTime' seconds.
-    public int maxFoodLevel = 2;
-    
-    // Maintains the amount of food in this square.
-    public int foodLevel = 2;
+    private bool foodAlive = true;
     
     // Incremented at every FixedUpdate, keeps track of time elapsed since last reset.
-    float timer = 0;
+    private float timer = 0;
 
     // Whenever the timer hits this value, food grows back.
     public float regrowTime = 10;
 
-    Sprite spriteAlive = null;
-    Sprite spriteDying = null;
-    Sprite spriteDead = null;
+    private Sprite spriteAlive = null;
+    private Sprite spriteDead = null;
 
     // Load all the sprites once, to speed things up during gameplay.
     // Anything placed in a folder called "Resources" is accessible through
     // the Resources.Load API call.
-    void Start() {
+    void Start()
+    {
         spriteAlive = Resources.Load<Sprite>("Food");
-        spriteDying = Resources.Load<Sprite>("DyingFood");
         spriteDead = Resources.Load<Sprite>("FoodDead");
     }
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         // Increment the elapsed time.
         timer += Time.deltaTime;
 
         // If this food hasn't been touched in 'regrowTime' seconds, increment.
         if (timer > regrowTime) {
-            IncrementFoodLevel();
+            RegrowFood();
         }
     }
 
     // Do any updates to the appearance before rendering.
     // TODO: running this at 60FPS is wasteful
-    void Update() {
+    void Update()
+    {
         UpdateSprite();
     }
 
     // Returns true if a ghost can eat this food patch.
     public bool IsEdible()
     {
-        return (foodLevel != 0);
+        return (foodAlive);
     }
 
     // Called whenever something enters the BoxCollider2D of the Food prefab.
-    void OnTriggerEnter2D(Collider2D co) {
+    void OnTriggerEnter2D(Collider2D co)
+    {
         // If PacManPlayer enters the food, decrement its level.
         if (co.tag == "Player") {
-            DecrementFoodLevel();
+            KillFood();
         }
     }
 
-    void IncrementFoodLevel() {
-        timer = 0; // Reset timer.
-        foodLevel += 1;
-        foodLevel = System.Math.Min(foodLevel, maxFoodLevel);
+    void RegrowFood()
+    {
+        foodAlive = true;
+        timer = 0;
     }
 
-    void DecrementFoodLevel() {
-        timer = 0; // Reset timer.
-        foodLevel -= 1;
-        foodLevel = System.Math.Max(foodLevel, 0);
+    void KillFood()
+    {
+        foodAlive = false;
+        timer = 0;
     }
 
     // Update the appearance of the food sprite based on its level.
-    void UpdateSprite() {
-        if (foodLevel == maxFoodLevel) {
+    void UpdateSprite()
+    {
+        if (foodAlive) {
             GetComponent<SpriteRenderer>().sprite = spriteAlive;
-        } else if (foodLevel == 0) {
-            GetComponent<SpriteRenderer>().sprite = spriteDead;
         } else {
-            GetComponent<SpriteRenderer>().sprite = spriteDying;
+            GetComponent<SpriteRenderer>().sprite = spriteDead;
         }
     }
 }
