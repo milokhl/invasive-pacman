4303f300da9634eb487466a105aa9eba60e97fa0
Milo Knowles <mknowles@mit.edu>
2019-02-25 01:01:06 -0500

    Working, but sketchy...

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -4,23 +4,41 @@ using UnityEngine;
 
 public class GhostController : MonoBehaviour
 {
-
     // Expose the speed parameter to Unity.
     public float speed = 0.1f;
     Vector2 dest = Vector2.zero;
+    Vector2 direction = new Vector2(1, 0);
 
     // Start is called before the first frame update
     void Start() {
-        dest = transform.position;
+        dest = (Vector2)transform.position;
+        direction = RandomDirection();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    Vector2 RandomDirection() {
+        List<Vector2> valid_directions = new List<Vector2>();
+        if (CollisionFree(new Vector2(0, 1))) {
+            valid_directions.Add(new Vector2(0, 1));
+        }
+        if (CollisionFree(new Vector2(0, -1))) {
+            valid_directions.Add(new Vector2(0, -1));
+        }
+        if (CollisionFree(new Vector2(1, 0))) {
+            valid_directions.Add(new Vector2(1, 0));
+        }
+        if (CollisionFree(new Vector2(-1, 0))) {
+            valid_directions.Add(new Vector2(-1, 0));
+        }
+
+        if (valid_directions.Count > 0) {
+            int choice = Random.Range(0, valid_directions.Count);
+            return valid_directions[choice];
+        }
+
+        return new Vector2(1, 0); // No valid directions found.
     }
 
     void FixedUpdate() {
-        Debug.Log((Vector2)transform.position + ", " + dest + ", ");
         // Move closer to the current destination.
         Vector2 updated_pos = Vector2.MoveTowards(transform.position, dest, speed);
 
@@ -28,18 +46,16 @@ public class GhostController : MonoBehaviour
         GetComponent<Rigidbody2D>().MovePosition(updated_pos);
 
         // Check for keyboard input if not moving (at destination).
-        if (((Vector2)transform.position - dest).SqrMagnitude() < 0.05)
-        {
+        if (((Vector2)transform.position - dest).SqrMagnitude() < 0.05) {
+        // if ((Vector2)transform.position == dest) {
             GetComponent<Rigidbody2D>().MovePosition(dest);
 
-            float randDirection = Random.Range(0, 2);
-            randDirection = 2 * (randDirection - 0.5f);
-            if (Random.Range(0, 2) == 0 && CollisionFree(-new Vector2(randDirection, 0)))
-            {
-                dest = (Vector2)transform.position - new Vector2(randDirection, 0);
-            } else if (CollisionFree(-new Vector2(0, randDirection)))
-            {
-                dest = (Vector2)transform.position - new Vector2(0, randDirection);
+            // If the direction of motion is free, keeping going that way.
+            if (CollisionFree(direction)) {
+                dest += direction;
+            } else {
+                // Otherwise, try to find a free direction to move in.
+                direction = RandomDirection();   
             }
         }
     }
